fc755cc78f6ab994625ba96d6a3e51e14eb67148
Celso Mireles <celso@digitalstrategy.tips>
2016-05-05 01:48:53 -0400

    Abstract away Sidebar from Home Layout

--- imports/ui/layouts/Main.jsx
+++ imports/ui/layouts/Main.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SideBar from '../components/SideBar.jsx';
 
 export default class Main extends Component {
 
@@ -10,11 +11,8 @@ export default class Main extends Component {
 	    };
 	}
 
-	sideNavClick(e){
-		e.preventDefault();
-		console.log("Side Nav button clicked.");
+	toggleSideNav(e){
 
-		//var sideToggle = this.state.sideToggle;
 		var sideNav = $('.side-nav');
 		var cosechaTech = $('#cosecha-tech');
 
@@ -23,14 +21,10 @@ export default class Main extends Component {
 		});
 
 		if(this.state.sideToggle){
-			console.log('Toggle On');
-			//sideNav.addClass('active');
 			cosechaTech.animate({
 				left: '250px'
 			});
 		}else {
-			console.log('Toggle Off');
-			//sideNav.removeClass('active');
 			cosechaTech.animate({
 				left: '0px'
 			});
@@ -51,7 +45,7 @@ export default class Main extends Component {
 					<div className="main-content">
 						<div className="page-container">
 							<header className="fixed-title">
-								<i className="fa fa-bars fa-2x" onClick={this.sideNavClick.bind(this)}></i>
+								<i className="fa fa-bars fa-2x" onClick={this.toggleSideNav.bind(this)}></i>
 								<h1>Room Title</h1>
 							</header>
 
@@ -59,32 +53,8 @@ export default class Main extends Component {
 						</div>
 					</div>
 
-					<aside className="side-nav">
-
-						<header className="header">
-							<div className="account-box">
-								<div className="info status-online">
-									<div data-status="online" className="thumb"><img src="http://placehold.it/40x40"/></div>
-									<div className="data">
-										<h4>celso.mireles</h4></div>
-								</div>
-								<nav className="options animated-hidden"></nav>
-							</div>
-						</header>
-
-						<div className="rooms-list">
-							<div className="wrapper">
-								<h3 className="add-room active">CHANNELS <i className="fa fa-plus"></i></h3>
-								<ul>
-									<li className="link-room"><a className="open-room"><i className="fa fa-hashtag"></i><span className="name"> general</span><span className="opt"></span></a></li>
-									<li className="link-room"><a><i className="fa fa-hashtag"></i><span className="name"> trainers</span><span className="opt"></span></a></li>
-									<li className="link-room"><a><i className="fa fa-hashtag"></i><span className="name"> artists</span><span className="opt"></span></a></li>
-									<li className="link-room"><a><i className="fa fa-hashtag"></i><span className="name"> arizona</span><span className="opt"></span></a></li>
-								</ul>
-							</div>
-						</div>
-						<footer className="footer"></footer>
-					</aside>
+					<SideBar />
+					
 
 				</div>
 			</div>
